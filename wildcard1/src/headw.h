c321be974a0b88fad05d35b0a193e8474656e85a
sushmithaleela <sushmitha.leela@gmail.com>
2016-06-02 12:48:35 +0530

    wildcard donehf

---
+++ wildcard1/src/headw.h
@@ -0,0 +1,17 @@
+/*
+
+
+ * headw.h
+ *
+ *  Created on: 02-Jun-2016
+ *      Author: Mayank Sharma
+ */
+
+#ifndef HEADW_H_
+#define HEADW_H_
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int match(char a[] ,char b[]);
+#endif /* HEADW_H_ */
