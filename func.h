8060a4a8733cd127bbef358e59742d722014c3a7
mayanksharmacfc <mayanksharmacfc@users.noreply.github.com>
2016-06-02 12:16:29 +0530

    Add files via upload

---
+++ func.h
@@ -0,0 +1,16 @@
+/**
+
+*	@file func.h
+*	@brief This file contains appropriate functions
+*	@author Mayank Sharma
+*	@date 2nd July 2016
+*/
+
+int revSpiral(); // function declaration
+
+
+
+
+
+
+
